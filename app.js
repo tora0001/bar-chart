3d572127bd213b595d8eb6cd414500a4b09ed3a1
tora0001 <tobe.2309@hotmail.com>
2023-04-12 10:05:50 +0200

    data

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ window.addEventListener("load", initApp);
 const data = [12, 9, 28, 4, 15, 26, 3, 19, 20, 7, 23, 16, 8, 5, 0, 27, 2, 25, 24, 29, 6, 10, 31, 17, 21, 11, 30, 13, 1, 18, 14, 22, 32, 31, 26, 5, 16, 13, 8, 2, 23, 29, 19];
 
 function initApp() {
-  displayAllBars();
+  setInterval(update,1000)
+}
+
+function update(){
+    updateData();
+    displayAllBars();
 }
 
 function displayAllBars() {
@@ -16,4 +21,9 @@ function displayAllBars() {
     let h = (data[i] / 32) * 100;
     bar.style.height = `${h}px`;
   }
+}
+
+function updateData(){
+    data.push(Math.floor(Math.random()32*100));
+    data.shift();
 }
