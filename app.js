0e9309e993da16e2843e5a7b43d65f27d0619550
tora0001 <tobe.2309@hotmail.com>
2023-04-12 09:57:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ window.addEventListener("load", initApp);
 const data = [12, 9, 28, 4, 15, 26, 3, 19, 20, 7, 23, 16, 8, 5, 0, 27, 2, 25, 24, 29, 6, 10, 31, 17, 21, 11, 30, 13, 1, 18, 14, 22, 32, 31, 26, 5, 16, 13, 8, 2, 23, 29, 19];
 
 function initApp() {
+  displayAllBars();
+}
+
+function displayAllBars() {
   const bars = document.querySelectorAll(".bar");
-  const firstBar = bars[0];
 
-  let h = (data[0] / 32) * 100;
-
-  firstBar.style.height = `${h}px`;
+  for (let i = 0; i < 40; i++) {
+    const bar = bars[i];
+    let h = (data[i] / 32) * 100;
+    bar.style.height = `${h}px`;
+  }
 }
