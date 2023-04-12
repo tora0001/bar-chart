7ca2e732b047ed3f5facdcf9f7214aa7c6b8a830
tora0001 <tobe.2309@hotmail.com>
2023-04-12 09:34:00 +0200

    changes height on first column

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 "use strict";
+
+window.addEventListener("load", initApp);
+
+function initApp() {
+  const bars = document.querySelectorAll(".bar");
+  const firstBar = bars[0];
+
+  firstBar.style.height = "50px";
+
+  let h = 50;
+
+  firstBar.style.height = `${h}px`;
+}
